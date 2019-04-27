8f4a2816481a56ac77746cca08a607b98a7b1cbe
USIJans <jansr@usi.ch>
2019-04-27 20:55:44 +0200

    determine position, velocity and acceleration

--- src/algorithms/PVT/gnuradio_blocks/feature_set.h
+++ src/algorithms/PVT/gnuradio_blocks/feature_set.h
@@ -1,13 +1,28 @@
 #ifndef FEATURE_SET_H   
 #define FEATURE_SET_H
 
-#include <map>
+
 #include "gnss_synchro.h"
 #include "rtklib_solver.h"
+#include <map>
+#include <ctime>
+
+#include <iostream>
+
+using namespace std::chrono;
+
+struct Position {
+
+    double latitude;
+    double longitude;
+    double height;
+};
 
 class FeatureSet {
 
 public:
+
+    // data mebers
 
     int nSats;
 //  int nSatellitesChanged
@@ -19,18 +34,22 @@ public:
 //  double eastingFromReal
 //  double northingFromReal
 //  double heightFromReal
-    double velocity;
-    double acceleration;
+    double velocity; // m/s
+    double acceleration; // m/s^2
     std::map<int, double> amplitudes;
 //  ___ timewithNoLock
 
+    // methods
 
     void assembleFeatures(std::map<int, Gnss_Synchro> &synchros, 
                           std::shared_ptr<rtklib_solver> pvt_solver) {
 
+        
+
         signalToNoise = 0;
         signalToNoise = 0;
         int counter = 0;
+        int dt = 0;
 
         for (auto it = synchros.begin(); it != synchros.end(); ++it) {
 
@@ -38,6 +57,13 @@ public:
 
             Gnss_Synchro syn = it->second;
             int32_t channel_id = syn.Channel_ID;
+
+            if (counter == 1) {
+
+                lastTime = currentTime;
+                currentTime = syn.TOW_at_current_symbol_ms;
+                dt = currentTime - lastTime;
+            }
                                 
             signalToNoise += syn.CN0_dB_hz;
             pseudoranges[channel_id] = syn.Pseudorange_m;
@@ -49,12 +75,26 @@ public:
         signalToNoise /= counter;
         dopplerMeasured /= counter;
         nSats = counter;
+
+        velocity = pvt_solver->get_speed_over_ground();
+        lastVelocity = currentVelocity;
+        currentVelocity = velocity;
+
+        lastPos = currentPos;
+        currentPos = {
+                        pvt_solver->get_latitude(),
+                        pvt_solver->get_longitude(),
+                        pvt_solver->get_height()
+                     };
+
+        acceleration = (velocity - lastVelocity) / (static_cast<double> (dt) / 1000.0);
     }
 
     void printFeatures() {
 
         std::cout   << "FEATURES:" << std::endl
                     << "  nSats: " << nSats << std::endl
+                    << "  satsChanged: " << "NA" << std::endl
                     << "  signalToNoise: " << signalToNoise << std::endl;
 
         printMap(pseudoranges, "pseudoranges");
@@ -65,13 +105,26 @@ public:
                     << "  eastingFromReal: " << "NA" << std::endl
                     << "  northingFromReal: " << "NA" << std::endl
                     << "  heightFromReal: " << "NA" << std::endl
-                    << "  velocity: " << "NA" << std::endl
-                    << "  acceleration: " << "NA" << std::endl;
+                    << "  velocity: " << velocity << " m/s" << std::endl
+                    << "  acceleration: " << acceleration << " m/s^2" << std::endl;
 
         printMap(amplitudes, "amplitudes");
     }
 
 private:
+
+    // data members
+
+    int lastTime; // times are in ms
+    int currentTime;
+
+    Position lastPos;
+    Position currentPos;
+
+    float lastVelocity;  // velocities in m/s
+    float currentVelocity;
+
+    // methods
 
     void printMap(std::map<int, double> m, std::string name) {
 
